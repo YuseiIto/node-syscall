4415f96df6d6fd4d8c3c5fcb4074aa750a06937a
Yusei Ito <me@yuseiito.com>
2021-07-25 18:32:41 +0900

    close, stat, fstat,fstat,lstat,poll,lseek

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include <napi.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <poll.h>
 
 namespace node_syscall {
 
@@ -52,6 +54,122 @@ Napi::Number node_open(const Napi::CallbackInfo& info)
 
 }
 
+Napi::Number node_close(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    long fd= info[0].As<Napi::Number>().Uint32Value();
+    return  Napi::Number::New(env,close(fd));
+
+}
+
+Napi::Object node_stat(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    std::string path = info[0].As<Napi::String>().Utf8Value();
+
+    struct stat* buf;
+    stat(path.c_str(),buf);
+    
+    Napi::Object obj = Napi::Object::New(env);
+    obj.Set("st_dev",Napi::Number::New(env,buf->st_dev));
+    obj.Set("st_ino",Napi::Number::New(env,buf->st_ino));
+    obj.Set("st_mode",Napi::Number::New(env,buf->st_mode));
+    obj.Set("st_nlink",Napi::Number::New(env,buf->st_nlink));
+    obj.Set("st_uid",Napi::Number::New(env,buf->st_uid));
+    obj.Set("st_gid",Napi::Number::New(env,buf->st_gid));
+    obj.Set("st_rdev",Napi::Number::New(env,buf->st_rdev));
+    obj.Set("st_size",Napi::Number::New(env,buf->st_size));
+    obj.Set("st_blksize",Napi::Number::New(env,buf->st_blksize));
+    obj.Set("st_blocks",Napi::Number::New(env,buf->st_blocks));
+    obj.Set("st_atime",Napi::Number::New(env,buf->st_atime));
+    obj.Set("st_mtime",Napi::Number::New(env,buf->st_mtime));
+    obj.Set("st_ctime",Napi::Number::New(env,buf->st_ctime));
+
+    return  obj;
+}
+
+
+Napi::Object node_lstat(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    std::string path = info[0].As<Napi::String>().Utf8Value();
+
+    struct stat* buf;
+    lstat(path.c_str(),buf);
+    
+    Napi::Object obj = Napi::Object::New(env);
+    obj.Set("st_dev",Napi::Number::New(env,buf->st_dev));
+    obj.Set("st_ino",Napi::Number::New(env,buf->st_ino));
+    obj.Set("st_mode",Napi::Number::New(env,buf->st_mode));
+    obj.Set("st_nlink",Napi::Number::New(env,buf->st_nlink));
+    obj.Set("st_uid",Napi::Number::New(env,buf->st_uid));
+    obj.Set("st_gid",Napi::Number::New(env,buf->st_gid));
+    obj.Set("st_rdev",Napi::Number::New(env,buf->st_rdev));
+    obj.Set("st_size",Napi::Number::New(env,buf->st_size));
+    obj.Set("st_blksize",Napi::Number::New(env,buf->st_blksize));
+    obj.Set("st_blocks",Napi::Number::New(env,buf->st_blocks));
+    obj.Set("st_atime",Napi::Number::New(env,buf->st_atime));
+    obj.Set("st_mtime",Napi::Number::New(env,buf->st_mtime));
+    obj.Set("st_ctime",Napi::Number::New(env,buf->st_ctime));
+
+    return  obj;
+}
+
+Napi::Object node_fstat(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    long fd = info[0].As<Napi::Number>().Uint32Value();
+
+    struct stat* buf;
+    fstat(fd,buf);
+    
+    Napi::Object obj = Napi::Object::New(env);
+    obj.Set("st_dev",Napi::Number::New(env,buf->st_dev));
+    obj.Set("st_ino",Napi::Number::New(env,buf->st_ino));
+    obj.Set("st_mode",Napi::Number::New(env,buf->st_mode));
+    obj.Set("st_nlink",Napi::Number::New(env,buf->st_nlink));
+    obj.Set("st_uid",Napi::Number::New(env,buf->st_uid));
+    obj.Set("st_gid",Napi::Number::New(env,buf->st_gid));
+    obj.Set("st_rdev",Napi::Number::New(env,buf->st_rdev));
+    obj.Set("st_size",Napi::Number::New(env,buf->st_size));
+    obj.Set("st_blksize",Napi::Number::New(env,buf->st_blksize));
+    obj.Set("st_blocks",Napi::Number::New(env,buf->st_blocks));
+    obj.Set("st_atime",Napi::Number::New(env,buf->st_atime));
+    obj.Set("st_mtime",Napi::Number::New(env,buf->st_mtime));
+    obj.Set("st_ctime",Napi::Number::New(env,buf->st_ctime));
+
+    return  obj;
+}
+
+Napi::Number node_poll(const Napi::CallbackInfo& info){
+
+    Napi::Env env = info.Env();
+
+    Napi::Array node_fds = info[0].As<Napi::Array>();
+    long nfds=info[1].As<Napi::Number>();
+    long timeout=info[2].As<Napi::Number>();
+    
+    pollfd* fds=(pollfd*) malloc(sizeof(pollfd)*nfds);
+
+    for (long i =0; i<nfds;i++){
+        Napi::Object e = node_fds.Get(i).As<Napi::Object>();
+        pollfd pfd;
+        pfd.fd=e.Get("fd").As<Napi::Number>();
+        pfd.fd=e.Get("events").As<Napi::Number>();
+        pfd.fd=e.Get("revents").As<Napi::Number>();
+        fds[i]=pfd;
+    }
+    return  Napi::Number::New(env,poll(fds,nfds,timeout));
+}
+
+Napi::Number node_lseek(const Napi::CallbackInfo& info){
+
+    Napi::Env env = info.Env();
+    long fields=info[0].As<Napi::Number>();
+    long offset=info[1].As<Napi::Number>();
+    long whence=info[2].As<Napi::Number>();
+    return  Napi::Number::New(env,lseek(fields,offset,whence));
+}
 
 Napi::Object Initialize(Napi::Env env, Napi::Object exports)
 {
@@ -63,7 +181,19 @@ Napi::Object Initialize(Napi::Env env, Napi::Object exports)
                 Napi::Function::New(env, node_write));
     exports.Set(Napi::String::New(env, "open"), 
                 Napi::Function::New(env, node_open));
-
+    exports.Set(Napi::String::New(env, "close"), 
+          Napi::Function::New(env, node_close));
+    exports.Set(Napi::String::New(env, "stat"), 
+          Napi::Function::New(env, node_stat));
+    exports.Set(Napi::String::New(env, "lstat"), 
+          Napi::Function::New(env, node_lstat));
+    exports.Set(Napi::String::New(env, "fstat"), 
+          Napi::Function::New(env, node_fstat));
+    exports.Set(Napi::String::New(env, "poll"), 
+          Napi::Function::New(env, node_poll));
+    exports.Set(Napi::String::New(env, "lseek"), 
+          Napi::Function::New(env, node_lseek));
+    
     return exports;
 }
 
