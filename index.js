1d686ea9d20b5d53d83c5580d11e8c98a53831c4
Yusei Ito <me@yuseiito.com>
2021-07-25 16:46:01 +0900

    Read, Open

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const syscall = require('./build/Release/node_syscall')
+const O_RDONLY=0;
 
 console.log(syscall.getpid());
+let fd=syscall.open("testdata.txt",0);
+console.log(fd);
+
+console.log(syscall.read(fd,10).toString('utf8'))
--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include <napi.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 namespace node_syscall {
 
@@ -11,10 +13,42 @@ Napi::Number node_getpid(const Napi::CallbackInfo& info)
     return  Napi::Number::New(env, pid);
 }
 
+Napi::Value node_read(const Napi::CallbackInfo& info)
+{
+    // read(fd,count) - > size
+
+    Napi::Env env = info.Env();
+    long fd= info[0].As<Napi::Number>().Uint32Value();
+    long count= info[1].As<Napi::Number>().Uint32Value();
+
+    uint8_t * buf=(uint8_t*) malloc(count);
+    size_t len=read(fd,buf,count);
+
+
+    return  Napi::Buffer<uint8_t>::Copy(env,buf,len);
+
+}
+
+Napi::Number node_open(const Napi::CallbackInfo& info)
+{
+    // open(path,mode) -> fd
+    Napi::Env env = info.Env();
+    std::string pathname_str= info[0].As<Napi::String>().Utf8Value();
+    const char* pathname=pathname_str.c_str();
+    mode_t mode = info[1].As<Napi::Number>().Uint32Value();
+    return  Napi::Number::New(env,open(pathname,mode));
+
+}
+
+
 Napi::Object Initialize(Napi::Env env, Napi::Object exports)
 {
     exports.Set(Napi::String::New(env, "getpid"), 
                 Napi::Function::New(env, node_getpid));
+    exports.Set(Napi::String::New(env, "read"), 
+                Napi::Function::New(env, node_read));
+    exports.Set(Napi::String::New(env, "open"), 
+                Napi::Function::New(env, node_open));
 
     return exports;
 }
